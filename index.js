37c843c794a2f83a5f178d27516de64c335ac85d
osovska-svitlana <109654003+osovska-svitlana@users.noreply.github.com>
2022-07-28 00:34:18 +0300

    working with icons
    made icons according the city weather

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let apiKey = "959764aef958aca1c60220a4c8d8110c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=959764aef958aca1c60220a4c8d8110c&units=metric`;
+let city = "Kyiv";  
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=959764aef958aca1c60220a4c8d8110c&units=metric`;
 
 function displayTemperature(responce) {
   console.log(responce);
@@ -15,6 +16,8 @@ function displayTemperature(responce) {
   windElement.innerHTML = Math.round(responce.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = actualDate(responce.data.dt * 1000); // actualDate -  новая функция, пишем ее, что бы сделать подсчет времени указаного города
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`)
 }
 function actualDate(timestamp) {
   let dateCurrent = new Date(timestamp); //new Date показывает актуальное время
