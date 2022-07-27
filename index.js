013e3db9d77e1e1445d91efb5c840bc74505d870
osovska-svitlana <109654003+osovska-svitlana@users.noreply.github.com>
2022-07-27 23:24:20 +0300

    Added weekdays and time
    Weekdays and time

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ let apiKey = "959764aef958aca1c60220a4c8d8110c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=959764aef958aca1c60220a4c8d8110c&units=metric`;
 
 function displayTemperature(responce) {
-    console.log(responce);
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(responce.data.main.temp);
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = responce.data.name;
-    let weatherConditionElement = document.querySelector("#weather-condition");
-    weatherConditionElement.innerHTML = responce.data.weather[0].description;
-    let humidityElement = document.querySelector("#humidity")
-    humidityElement.innerHTML = responce.data.main.humidity;
-    let windElement = document.querySelector("#wind-speed");
-    windElement.innerHTML = Math.round(responce.data.wind.speed);
-    
+  console.log(responce);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(responce.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = responce.data.name;
+  let weatherConditionElement = document.querySelector("#weather-condition");
+  weatherConditionElement.innerHTML = responce.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = responce.data.main.humidity;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = Math.round(responce.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = actualDate(responce.data.dt * 1000); // actualDate -  новая функция, пишем ее, что бы сделать подсчет времени указаного города
+}
+function actualDate(timestamp) {
+  let dateCurrent = new Date(timestamp); //new Date показывает актуальное время
+  let hours = dateCurrent.getHours();
+  let minutes = dateCurrent.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  } 
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days =  day [dateCurrent.getDay()];
+  return `${days}  ${hours}:${minutes}`;
 }
 
 axios.get(apiUrl).then(displayTemperature);
+ 
