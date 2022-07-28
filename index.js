bdc103bdb6829e1dcfde0599e17bc1fb8c71ff3c
osovska-svitlana <109654003+osovska-svitlana@users.noreply.github.com>
2022-07-28 23:26:07 +0300

    fahrenheit and celsius links added
    added fahrenheit and celsius links

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
-
 function displayTemperature(responce) {
   console.log(responce);
+
+  celsiusTemperature = responce.data.main.temp;
+
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(responce.data.main.temp);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = responce.data.name;
   let weatherConditionElement = document.querySelector("#weather-condition");
@@ -15,7 +17,10 @@ function displayTemperature(responce) {
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = actualDate(responce.data.dt * 1000); // actualDate -  новая функция, пишем ее, что бы сделать подсчет времени указаного города
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`)
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`
+  );
 }
 function actualDate(timestamp) {
   let dateCurrent = new Date(timestamp); //new Date показывает актуальное время
@@ -23,29 +28,55 @@ function actualDate(timestamp) {
   let minutes = dateCurrent.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
-  } 
+  }
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let days =  day [dateCurrent.getDay()];
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let days = day[dateCurrent.getDay()];
   return `${days}  ${hours}:${minutes}`;
 }
 
 function search(city) {
-let apiKey = "959764aef958aca1c60220a4c8d8110c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=959764aef958aca1c60220a4c8d8110c&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-
+  let apiKey = "959764aef958aca1c60220a4c8d8110c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=959764aef958aca1c60220a4c8d8110c&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
-  
+
 function handleSubbmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsuisTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+search("New York");
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#searchline");
 form.addEventListener("submit", handleSubbmit); // handleSubbmit - a function;
- 
+
+let fahrenheitElement = document.querySelector("#fahrenheit");
+fahrenheitElement.addEventListener("click", displayFahrenheitTemperature);
+let celsiusElement = document.querySelector("#celsius");
+celsiusElement.addEventListener("click", displayCelsuisTemperature);
